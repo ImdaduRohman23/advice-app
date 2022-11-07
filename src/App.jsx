1fabb9326ff3fe069f6f85bc5dd1038033d004af
ImdaduRohman23 <ir.imdadurohman@gmail.com>
2022-11-07 18:11:22 +0700

    fetch api

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [advice, setAdvice] = useState();
+
+  const fetchData = () => {
+    axios.get('https://api.adviceslip.com/advice')
+      .then(res => setAdvice(res.data.slip.advice))
+      .catch(err => console.log(err))
+
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <h1>Advice App</h1>
+      <h2>{advice}</h2>
     </div>
   );
 }
