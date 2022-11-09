42c946a2999ea42a5b039984498ce2b475d5fc07
ImdaduRohman23 <ir.imdadurohman@gmail.com>
2022-11-09 08:20:37 +0700

    update z index footer and button

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 
 function App() {
@@ -15,11 +15,48 @@ function App() {
     fetchData();
   }, []);
 
+  const buttonRef = useRef();
+
+  useEffect(() => {
+    const applyContainerProperties = () => {
+      buttonRef.current.classList.add("effect-container");
+    };
+
+    const applyStyles = (e) => {
+      const {offsetX, offsetY} = e;
+      const {style} = buttonRef.current;
+      const sizeOffset = 50;
+
+      style.setProperty("--effect-top", `${offsetY - sizeOffset}px`);
+      style.setProperty("--effect-left", `${offsetX - sizeOffset}px`);
+    }
+
+    const onClick = (e) => {
+      buttonRef.current.classList.remove("active");
+      applyStyles(e);
+      buttonRef.current.classList.add("active");
+    };
+
+    applyContainerProperties();
+
+    buttonRef.current.addEventListener("mouseup", onClick);
+
+    const cleanupRef = buttonRef.current;
+    return () => {
+      cleanupRef.removeEventListener("mouseup", onClick)
+    };
+  });
+
+
+
   return (
     <div className="app">
       <div className="app__content">
         <h1>{advice}</h1>
-        <a href="#" onClick={() => fetchData()}><span>GIVE ME ADVICE</span></a>
+        <div className="button-container" ref={buttonRef} onClick={fetchData}>
+          <button><strong>GIVE ME ADVICE</strong></button>
+        </div>
+        {/* <a href="#" onClick={() => fetchData()}><span>GIVE ME ADVICE</span></a> */}
       </div>
       <div className="footer">
         <p> <strong>&copy; 2022 Imdadu Rohman</strong> </p>
